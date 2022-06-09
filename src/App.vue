10fb89484f61211b66de5be358c738d597053e35
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-06-09 23:35:59 +0100

    feat: deleted an api key
    BREAKING CHANGE:

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">{{dateBuilder()}}</div>
         </div>
-
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}} °c</div>
           <div class="weather">{{weather.weather[0].main}}</div>
@@ -31,7 +30,7 @@ export default {
   name: 'app',
   data() {
     return {
-      api_key: '77854068949f3f3b8a9c6bcd807b0a70',
+      api_key: '',
       url_base: 'https://api.openweathermap.org/data/2.5/', 
       query: '',
       weather: {}
@@ -40,7 +39,7 @@ export default {
   methods: {
     fetchWeather(e){
       if (e.key == "Enter"){
-        // Example: http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0846f0e6eba725cf487d9a7e44f28fe7
+        // Example: http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cccccccccccccd9a7e44f28fe7
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
